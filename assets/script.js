bdc9b6b3f6b8214de23d8c25e175d937e96329a0
Thomas Maxey <tmaxey201@gmail.com>
2021-05-22 00:00:21 -0400

    added second area for portfolio work

--- assets/script.js
+++ assets/script.js
@@ -32,54 +32,50 @@ function myFunction() {
 
 // function to fade in and fade out divs 
 $(document).ready(function() {
-  window.setTimeout("fadeMyDiv();", 2000); //call fade in 2 seconds for #fade
+  window.setTimeout("fadeMyDiv();", 1000); //call fade in 2 seconds for #fade
 }
 )
 
 function fadeMyDiv() { //fadeout #fade
-  $("#fade").fadeOut(2000);
+  $("#fade").fadeOut(1500);
 }
 
 setTimeout(function(){
-  $('#fade1').fadeIn(3000);  //fade in fade1
-}, 3500);
+  $('#fade1').fadeIn(2000);  //fade in fade1
+}, 1000);
 
-// $(document).ready(function() {
-//   window.setTimeout("fadeMyDiv1();", 2000); //call fade in 2 seconds for #fade1
-// }
-// )
 
 
 function fadeMyDiv1() {
-  $("#fade1").fadeOut(2000);  //fade out fade1
+  $("#fade1").fadeOut(1500);  //fade out fade1
 }
 
-setTimeout(function(){
-  $('#fade2').fadeIn(3000);  
-}, 8000);
-
 $(document).ready(function() {
-  window.setTimeout("fadeMyDiv2();", 11000); //call fade in 3 seconds
+  window.setTimeout("fadeMyDiv1();", 1000); 
 }
 )
 
 function fadeMyDiv2() {
-  $("#fade2").fadeOut(2000);  //fade out fade1
+  $("#fade2").fadeOut(1500);  //fade out fade1
 }
 
-
-
 $(document).ready(function() {
-  window.setTimeout("fadeMyDiv1();", 6000); 
+  window.setTimeout("fadeMyDiv2();", 3000); //call fade in 3 seconds
 }
 )
 
+
 setTimeout(function(){
-  $('#fade3').fadeIn(3000);  
-}, 12500);
+  $('#fade2').fadeIn(1000);    
+}, 3000);
+
+
+setTimeout(function(){
+  $('#fade3').fadeIn(2000);  
+}, 4000);
 
 $(document).ready(function() {
-  window.setTimeout("fadeMyDiv3();", 14500); //call fade in 3 seconds
+  window.setTimeout("fadeMyDiv3();", 4000); //call fade in 3 seconds
 }
 )
 
@@ -88,19 +84,19 @@ function fadeMyDiv3() {
 }
 setTimeout(function(){
   $('#fade4').fadeIn(3000);  
-}, 17000);
+}, 6000);
 
 setTimeout(function(){
   $('#fade5').fadeIn(3000);  
-}, 18750);
+}, 7500);
 
 setTimeout(function(){
   $('#fade6').fadeIn(3000);  
-}, 20500);
+}, 9000);
 
 setTimeout(function(){
   $('#fade7').fadeIn(1500);  
-}, 22000);
+}, 11000);
 
 // onclick transition   onclick to add class!
 // $(function() {                      
@@ -123,12 +119,30 @@ $(function(){
 });
 
 
-$('.trigger, .slider').click(function() {
-  $('.slider').toggleClass('close');
+$('.trigger').click(function() {
+  $('.slider').removeClass('close');
 });
+
+
+$('#mywork').click(function() {
+  $('.slider').addClass('close');
+});
+
+$('#mywork').click(function() {
+  $('.slider').removeClass('close');
+  $('.newdiv').removeClass('hidemenow');
+});
+
+$('#aboutme').click(function() {
+  $('.newdiv').addClass('hidemenow');
+ 
+});
+
+
 
 $(document).ready(function(){
   $(".trigger").click(function(){
      $(".add_here").removeClass("slider1");
   });
 });
+
